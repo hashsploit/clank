8cda3e3bd12484a87f7170d7e80231af0ef0d49f
hashsploit <hashsploit@protonmail.com>
2020-09-20 02:33:25 -0700

    Updated linux build and launch scripts

--- build.sh
+++ build.sh
@@ -2,14 +2,16 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Check if maven is installed
 if ! command -v mvn &> /dev/null; then
     echo -e "$(tput bold)$(tput setaf 1)Maven is not installed, please install maven!$(tput sgr0)"
     exit 1
 fi
 
+# Compile the jar file
 echo -e "Building ..."
-
 mvn clean compile assembly:single
 
+# Copy final jar to root path.
 cp target/clank-*.jar clank.jar
 
