02532999d8fe31599c3525c6338e1ccc5ea2012a
hashsploit <hashsploit@protonmail.com>
2020-09-24 20:16:55 -0700

    Updated CLI commands

--- src/main/java/net/hashsploit/clank/cli/commands/CLIClientsCommand.java
+++ src/main/java/net/hashsploit/clank/cli/commands/CLIClientsCommand.java
@@ -1,21 +1,18 @@
 package net.hashsploit.clank.cli.commands;
 
-import java.util.logging.Level;
-
-import net.hashsploit.clank.Clank;
-import net.hashsploit.clank.MediusComponent;
+import net.hashsploit.clank.ServerComponent;
 import net.hashsploit.clank.Terminal;
 import net.hashsploit.clank.cli.ICLICommand;
-import net.hashsploit.clank.server.Client;
 
 public class CLIClientsCommand implements ICLICommand {
 	
 	@Override
 	public void invoke(Terminal term, String[] params) {
-		term.print(Level.INFO, "Clients: " + Clank.getInstance().getServer().getClients());
-		for (Client client : Clank.getInstance().getServer().getClients()) {
-			term.print(Level.INFO, String.format("- %s:%d -> %s", client.getIPAddress(), client.getPort(), client.getClientState().toString()));	
-		}
+		// FIXME: broken
+		//term.print(Level.INFO, "Clients: " + Clank.getInstance().getServer().getClients());
+		//for (IClient client : Clank.getInstance().getServer().getClients()) {
+		//	term.print(Level.INFO, String.format("- %s:%d -> %s", client.getIPAddress(), client.getPort(), client.getClientState().toString()));	
+		//}
 	}
 	
 	@Override
@@ -31,7 +28,7 @@ public class CLIClientsCommand implements ICLICommand {
 	@Override
 	public int enabledMediusModes() {
 		int value = 0;
-		for (MediusComponent m : MediusComponent.values()) {
+		for (ServerComponent m : ServerComponent.values()) {
 			value |= m.getModeId();
 		}
 		return value;
--- src/main/java/net/hashsploit/clank/cli/commands/CLIExitCommand.java
+++ src/main/java/net/hashsploit/clank/cli/commands/CLIExitCommand.java
@@ -1,6 +1,6 @@
 package net.hashsploit.clank.cli.commands;
 
-import net.hashsploit.clank.MediusComponent;
+import net.hashsploit.clank.ServerComponent;
 import net.hashsploit.clank.Clank;
 import net.hashsploit.clank.Terminal;
 import net.hashsploit.clank.cli.ICLICommand;
@@ -25,7 +25,7 @@ public class CLIExitCommand implements ICLICommand {
 	@Override
 	public int enabledMediusModes() {
 		int value = 0;
-		for (MediusComponent m : MediusComponent.values()) {
+		for (ServerComponent m : ServerComponent.values()) {
 			value |= m.getModeId();
 		}
 		return value;
