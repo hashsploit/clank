07ca5bf51f27e2348a130e60087c7ebdd119af80
hashsploit <hashsploit@protonmail.com>
2020-10-28 18:28:20 -0700

    Added VersionServer request/response and handler

---
+++ src/main/java/net/hashsploit/clank/server/medius/packets/serializers/VersionServerRequest.java
@@ -0,0 +1,38 @@
+package net.hashsploit.clank.server.medius.packets.serializers;
+
+import java.nio.ByteBuffer;
+
+import net.hashsploit.clank.server.medius.MediusConstants;
+import net.hashsploit.clank.server.medius.MediusPacketType;
+import net.hashsploit.clank.server.medius.objects.MediusPacket;
+
+public class VersionServerRequest extends MediusPacket {
+
+	private byte[] messageId = new byte[MediusConstants.MESSAGEID_MAXLEN.getValue()];
+	private byte[] sessionKey = new byte[MediusConstants.SESSIONKEY_MAXLEN.getValue()];
+	
+	public VersionServerRequest(byte[] data) {
+		super(MediusPacketType.VersionServer, data);
+		
+		ByteBuffer buf = ByteBuffer.wrap(data);
+		buf.get(messageId);
+		buf.get(sessionKey);
+	}
+	
+	public byte[] getMessageId() {
+		return messageId;
+	}
+
+	public void setMessageId(byte[] messageId) {
+		this.messageId = messageId;
+	}
+
+	public byte[] getSessionKey() {
+		return sessionKey;
+	}
+
+	public void setSessionKey(byte[] sessionKey) {
+		this.sessionKey = sessionKey;
+	}
+	
+}
