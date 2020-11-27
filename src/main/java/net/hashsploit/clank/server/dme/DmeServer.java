5afff6a1631762d979986230fb0e1647c7aa305e
hashsploit <hashsploit@protonmail.com>
2020-11-27 00:36:11 -0800

    Formatted DMEServer

--- src/main/java/net/hashsploit/clank/server/dme/DmeServer.java
+++ src/main/java/net/hashsploit/clank/server/dme/DmeServer.java
@@ -12,44 +12,42 @@ import net.hashsploit.clank.server.TcpServer;
 import net.hashsploit.clank.server.UdpServer;
 
 public class DmeServer extends TcpServer {
-	
+
 	private static final Logger logger = Logger.getLogger(DmeServer.class.getName());
-	
+
 	private final String udpAddress;
 	private final int udpStartingPort;
 	private final int udpThreads;
 	private HashSet<UdpServer> gameServers;
-	
+
 	public DmeServer(final String tcpAddress, final int tcpPort, final int tcpParentThreads, final int tcpChildThreads, final String udpAddress, final int udpStartingPort, final int udpThreads) {
 		super(tcpAddress, tcpPort, tcpParentThreads, tcpChildThreads);
-		
+
 		this.udpAddress = udpAddress;
 		this.udpStartingPort = udpStartingPort;
 		this.udpThreads = udpThreads;
 		this.gameServers = new HashSet<UdpServer>();
-		
+
 		String udpServerAddress = ((DmeConfig) Clank.getInstance().getConfig()).getUdpAddress();
 		int udpServerPort = 50001;
-		EventLoopGroup udpEventLoopGroup =  new EpollEventLoopGroup(2);
 
+		EventLoopGroup udpEventLoopGroup = new EpollEventLoopGroup(2);
 		Executors.newSingleThreadExecutor().execute(() -> { // TODO: this is super tempoarary
 			UdpServer udpDmeServer = new UdpServer(udpServerAddress, udpServerPort, udpEventLoopGroup);
 			gameServers.add(udpDmeServer);
 			udpDmeServer.setChannelInitializer(new DmeUdpClientInitializer(udpDmeServer));
 			udpDmeServer.start();
 		});
-		
+
 		setChannelInitializer(new DmeTcpClientInitializer(this));
 	}
-	
+
 	@Override
 	public void start() {
 		super.start();
-		
-		
+
 	}
-	
-	
+
 	@Override
 	public void stop() {
 		logger.fine("Shutting down DME UDP game servers ...");
