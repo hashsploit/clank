88bbfdb9f436d5c8e0071f9e649a3fb2e7103a22
jtjanecek <janecektyler@gmail.com>
2020-12-06 00:33:35 -0800

    BUG: Wasn't grabbing UDP Starting port

--- src/main/java/net/hashsploit/clank/server/dme/DmeServer.java
+++ src/main/java/net/hashsploit/clank/server/dme/DmeServer.java
@@ -33,7 +33,7 @@ public class DmeServer extends TcpServer {
 		this.gameServers = new HashSet<UdpServer>();
 
 		String udpServerAddress = ((DmeConfig) Clank.getInstance().getConfig()).getUdpAddress();
-		int udpServerPort = 50001;
+		int udpServerPort = ((DmeConfig) Clank.getInstance().getConfig()).getUdpStartingPort() + 1;
 
 		EventLoopGroup udpEventLoopGroup = new EpollEventLoopGroup(2);
 		Executors.newSingleThreadExecutor().execute(() -> { // TODO: this is super tempoarary
