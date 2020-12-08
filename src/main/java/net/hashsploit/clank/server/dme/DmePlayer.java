d0f4ca8636edb309fb9cdeff09fc67d950c97db8
hashsploit <hashsploit@protonmail.com>
2020-12-07 20:17:45 -0800

    Switched to PriorityBlockingQueue

--- src/main/java/net/hashsploit/clank/server/dme/DmePlayer.java
+++ src/main/java/net/hashsploit/clank/server/dme/DmePlayer.java
@@ -3,15 +3,10 @@ package net.hashsploit.clank.server.dme;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.SocketChannel;
 import net.hashsploit.clank.server.common.objects.DmePlayerStatus;
@@ -19,7 +14,7 @@ import net.hashsploit.clank.server.common.objects.DmePlayerStatus;
 public class DmePlayer {
 
 	private int playerId;
-	
+
 	// TCP
 	private SocketChannel socket;
 	// UDP
@@ -27,11 +22,15 @@ public class DmePlayer {
 	private InetSocketAddress playerUdpAddr;
 	private int aggTime = 30; // in ms
 	private float lastSendTime;
+	private PriorityBlockingQueue<byte[]> packetQueue;
 
-    BlockingQueue<byte[]> packetQueue = new LinkedBlockingQueue<>();	
-	
+	public DmePlayer() {
+		packetQueue = new PriorityBlockingQueue<byte[]>();
+		
+	}
+
 	private DmePlayerStatus status = DmePlayerStatus.DISCONNECTED;
-	
+
 	public DmePlayer(int playerId, SocketChannel socket) {
 		status = DmePlayerStatus.CONNECTING;
 		this.playerId = playerId;
@@ -61,29 +60,27 @@ public class DmePlayer {
 	}
 
 	public void sendUdpData(byte[] payload) {
-		//playerUdpChannel.writeAndFlush(new DatagramPacket(Unpooled.copiedBuffer(payload), playerUdpAddr));
+		// playerUdpChannel.writeAndFlush(new
+		// DatagramPacket(Unpooled.copiedBuffer(payload), playerUdpAddr));
 		packetQueue.add(payload);
 	}
-	
-	
+
 	public void flushUdpData() {
-	    ByteArrayOutputStream out = new ByteArrayOutputStream();
-	    
-	    // Gets an instance of the queue size, removes that # of packets (so even if more are added,
-	    // they won't be pop'd until next call)
-    	try {
-		    for (int i = 0; i < packetQueue.size(); i++) {
-		    	out.write(packetQueue.take());
-		    }
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		// Gets an instance of the queue size, removes that # of packets (so even if
+		// more are added,
+		// they won't be pop'd until next call)
+		try {
+			for (int i = 0; i < packetQueue.size(); i++) {
+				out.write(packetQueue.poll());
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
-    	
+
 		playerUdpChannel.writeAndFlush(new DatagramPacket(Unpooled.copiedBuffer(out.toByteArray()), playerUdpAddr));
 	}
-	
+
 }
