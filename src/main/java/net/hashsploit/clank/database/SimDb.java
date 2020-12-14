d24bc6fb816f94aa7d4a0253cc090da2d9895130
jtjanecek <janecektyler@gmail.com>
2020-12-13 16:15:53 -0800

    Cleaned up SimDB. Easier to add alpha testers with Triplet

--- src/main/java/net/hashsploit/clank/database/SimDb.java
+++ src/main/java/net/hashsploit/clank/database/SimDb.java
@@ -1,13 +1,41 @@
 package net.hashsploit.clank.database;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 public class SimDb implements IDatabase {
 	
 	public static final Logger logger = Logger.getLogger(SimDb.class.getName());
-		
+	
+	private static final ArrayList<Triplet<Integer, String, String>> users = new ArrayList<Triplet<Integer, String, String>>();
+
+
+	public class Triplet<T, U, V> {
+	    private final T first;
+	    private final U second;
+	    private final V third;
+	    public Triplet(T first, U second, V third) {
+	        this.first = first;
+	        this.second = second;
+	        this.third = third;
+	    }
+	    public T getFirst() { return first; }
+	    public U getSecond() { return second; }
+	    public V getThird() { return third; }
+	}
+
+	
+	
 	
 	public SimDb() {
+		users.add(new Triplet(50, "Smily", "11111111111111111111111111111111"));
+		users.add(new Triplet(51, "hashsploit", "22222222222222222222222222222222"));
+		users.add(new Triplet(52, "FourBolt", "33333333333333333333333333333333"));
+		users.add(new Triplet(53, "aequs", "44444444444444444444444444444444"));
+		users.add(new Triplet(54, "trop", "55555555555555555555555555555555"));
+		users.add(new Triplet(55, "Badger41", "66666666666666666666666666666666"));
+		users.add(new Triplet(56, "Dnawrkshp", "77777777777777777777777777777777"));
+		
 		logger.info("Simulated DB initialized ...");
 	}
 
@@ -25,98 +53,41 @@ public class SimDb implements IDatabase {
 	
 	@Override
 	public int getAccountId(String username) {
-		if (username.toLowerCase().equals("smily")) {
-			return 50;
+		for (Triplet user: users) {
+			if (username.toLowerCase().equals(((String) user.getSecond()).toLowerCase())) {
+				return (int) user.getFirst();
+			}
 		}
-		else if (username.toLowerCase().equals("hashsploit")) {
-			return 51;
-		}
-		else if (username.toLowerCase().equals("fourbolt")) { // Used in MediusGetMyClansHandler
-			return 52;
-		}
-		else if (username.toLowerCase().equals("aequs")) {
-			return 53;
-		}
-		else if (username.toLowerCase().equals("trop")) {
-			return 54;
-		}
-		else if (username.toLowerCase().equals("badger41")) {
-			return 55;
-		}
-		else if (username.toLowerCase().equals("dan")) {
-			return 56;
-		}
-		
 		return 0;
 	}
 	
 	@Override
 	public String getMlsToken(Integer accountId) {
-		switch(accountId) {
-		case 50:
-			return "11111111111111111111111111111111";
-		case 51:
-			return "22222222222222222222222222222222";
-		case 52:
-			return "33333333333333333333333333333333";
-		case 53:
-			return "44444444444444444444444444444444";
-		case 54:
-			return "55555555555555555555555555555555";
-		case 55:
-			return "66666666666666666666666666666666";
-		case 56:
-			return "77777777777777777777777777777777";
+		for (Triplet user: users) {
+			if (accountId == (int) user.getFirst()) {
+				return (String) user.getThird();
+			}
 		}
-		
 		return null;
 	}
 	
 	@Override
 	public int getAccountIdFromMlsToken(String mlsToken) {
-		if (mlsToken.equals("11111111111111111111111111111111")) {
-			return 50;
-		}
-		if (mlsToken.equals("22222222222222222222222222222222")) {
-			return 51;
-		}
-		if (mlsToken.equals("33333333333333333333333333333333")) {
-			return 52;
-		}
-		if (mlsToken.equals("44444444444444444444444444444444")) {
-			return 53;
-		}
-		if (mlsToken.equals("55555555555555555555555555555555")) {
-			return 54;
-		}
-		if (mlsToken.equals("66666666666666666666666666666666")) {
-			return 55;
-		}
-		if (mlsToken.equals("77777777777777777777777777777777")) {
-			return 56;
+		for (Triplet user: users) {
+			if (mlsToken.equals((String) user.getThird())) {
+				return (int) user.getFirst();
+			}
 		}
 		return -1;
 	}
 
 	@Override
 	public String getUsername(int accountId) {
-		switch(accountId) {
-		case 50:
-			return "smily";
-		case 51:
-			return "hashsploit";
-		case 52:
-			return "fourbolt";
-		case 53:
-			return "aequs";
-		case 54:
-			return "trop";
-		case 55:
-			return "badger41";
-		case 56:
-			return "dan";
+		for (Triplet user: users) {
+			if (accountId == (int) user.getFirst()) {
+				return (String) user.getSecond();
+			}
 		}
-		
 		return null;
 	}
 
