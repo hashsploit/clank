76ebc9d85d45fd9aa9408ca75a0a67eec3bee6b8
hashsploit <hashsploit@protonmail.com>
2020-12-13 18:41:47 -0800

    Temporary fix to force disconnect client

--- src/main/java/net/hashsploit/clank/server/common/packets/handlers/MediusAccountLogoutHandler.java
+++ src/main/java/net/hashsploit/clank/server/common/packets/handlers/MediusAccountLogoutHandler.java
@@ -1,6 +1,8 @@
 package net.hashsploit.clank.server.common.packets.handlers;
 
 import net.hashsploit.clank.server.MediusClient;
+import net.hashsploit.clank.server.RTMessage;
+import net.hashsploit.clank.server.RTMessageId;
 import net.hashsploit.clank.server.common.MediusPacketHandler;
 import net.hashsploit.clank.server.common.MediusMessageType;
 import net.hashsploit.clank.server.common.objects.MediusMessage;
@@ -22,6 +24,8 @@ public class MediusAccountLogoutHandler extends MediusPacketHandler {
 
 	@Override
 	public void write(MediusClient client) {
+		client.sendMessage(new RTMessage(RTMessageId.SERVER_FORCED_DISCONNECT, new byte[] {0x00}));
+		client.disconnect();
 	}
 
 }
