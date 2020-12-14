2a08333806f8db4c65295b439dbfe51ddb2d6d64
hashsploit <hashsploit@protonmail.com>
2020-12-13 18:44:12 -0800

    Cleaned up imports

--- src/main/java/net/hashsploit/clank/server/common/packets/handlers/MediusAccountLogoutHandler.java
+++ src/main/java/net/hashsploit/clank/server/common/packets/handlers/MediusAccountLogoutHandler.java
@@ -3,12 +3,10 @@ package net.hashsploit.clank.server.common.packets.handlers;
 import net.hashsploit.clank.server.MediusClient;
 import net.hashsploit.clank.server.RTMessage;
 import net.hashsploit.clank.server.RTMessageId;
-import net.hashsploit.clank.server.common.MediusPacketHandler;
 import net.hashsploit.clank.server.common.MediusMessageType;
+import net.hashsploit.clank.server.common.MediusPacketHandler;
 import net.hashsploit.clank.server.common.objects.MediusMessage;
 import net.hashsploit.clank.server.common.packets.serializers.AccountLogoutRequest;
-import net.hashsploit.clank.server.common.packets.serializers.GameInfoZeroRequest;
-import net.hashsploit.clank.utils.Utils;
 
 public class MediusAccountLogoutHandler extends MediusPacketHandler {
 	private AccountLogoutRequest reqPacket;
