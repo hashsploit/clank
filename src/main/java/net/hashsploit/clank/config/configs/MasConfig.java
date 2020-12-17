48e3da09fcc663638c23268b35c67a28049eb889
hashsploit <hashsploit@protonmail.com>
2020-12-17 13:39:07 -0800

    Added MediusGetLocationsHandler support for MAS

--- src/main/java/net/hashsploit/clank/config/configs/MasConfig.java
+++ src/main/java/net/hashsploit/clank/config/configs/MasConfig.java
@@ -1,12 +1,16 @@
 package net.hashsploit.clank.config.configs;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import net.hashsploit.clank.EmulationMode;
 import net.hashsploit.clank.config.ConfigNames;
+import net.hashsploit.clank.config.objects.LocationConfig;
 
 public class MasConfig extends MediusConfig {
 
@@ -75,6 +79,42 @@ public class MasConfig extends MediusConfig {
 	@Override
 	public EmulationMode getEmulationMode() {
 		return EmulationMode.MEDIUS_AUTHENTICATION_SERVER;
+	}
+
+	/**
+	 * Get a list of available locations.
+	 * @return
+	 */
+	public List<LocationConfig> getLocations() {
+		final String key = ConfigNames.LOCATIONS.toString();
+		final List<LocationConfig> locations = new ArrayList<LocationConfig>();
+		
+		if (getJson().isNull(key)) {
+			return null;
+		}
+		
+		if (getJson().getJSONArray(key).isEmpty()) {
+			return null;
+		}
+		
+		final JSONArray jsonArray = getJson().getJSONArray(key);
+		final Iterator<Object> iterator = jsonArray.iterator();
+		
+		while (iterator.hasNext()) {
+			final Object obj = iterator.next();
+			if (obj instanceof JSONObject) {
+				final JSONObject jsonObj = (JSONObject) obj;
+				if (jsonObj.has(ConfigNames.LOCATIONS_ID.toString()) && jsonObj.has(ConfigNames.LOCATIONS_NAME.toString())) {
+					
+					final int id = jsonObj.getInt(ConfigNames.LOCATIONS_ID.toString());
+					final String name = jsonObj.getString(ConfigNames.LOCATIONS_NAME.toString());
+					
+					locations.add(new LocationConfig(id, name));
+				}
+			}
+		}
+		
+		return locations;
 	}
 
 }
