23299178b2dd579246f356e0fb00bf0d4866b773
Joey743 <joseph.natoli12@gmail.com>
2021-01-03 17:53:10 -0500

    Implement SetLocalizationParams

--- src/main/java/net/hashsploit/clank/server/common/packets/handlers/MediusSetLocalizationParamsHandler.java
+++ src/main/java/net/hashsploit/clank/server/common/packets/handlers/MediusSetLocalizationParamsHandler.java
@@ -1,22 +1,17 @@
 package net.hashsploit.clank.server.common.packets.handlers;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-
 import net.hashsploit.clank.server.MediusClient;
-import net.hashsploit.clank.server.common.MediusConstants;
+import net.hashsploit.clank.server.common.MediusCallbackStatus;
 import net.hashsploit.clank.server.common.MediusPacketHandler;
 import net.hashsploit.clank.server.common.MediusMessageType;
 import net.hashsploit.clank.server.common.objects.MediusMessage;
-import net.hashsploit.clank.utils.Utils;
+import net.hashsploit.clank.server.common.packets.serializers.SetLocalizationParamsRequest;
+import net.hashsploit.clank.server.common.packets.serializers.SetLocalizationParamsResponse;
 
 public class MediusSetLocalizationParamsHandler extends MediusPacketHandler {
 
-	private byte[] messageID = new byte[MediusConstants.MESSAGEID_MAXLEN.value];
-	private byte[] sessionKey = new byte[MediusConstants.SESSIONKEY_MAXLEN.value];
-	private byte[] characterEncoding = new byte[4];
-	private byte[] language = new byte[4];
+	private SetLocalizationParamsRequest reqPacket;
+	private SetLocalizationParamsResponse respPacket;
 	
 	public MediusSetLocalizationParamsHandler() {
 		super(MediusMessageType.SetLocalizationParams, MediusMessageType.SetLocalizationParamsResponse);
@@ -24,28 +19,18 @@ public class MediusSetLocalizationParamsHandler extends MediusPacketHandler {
 	
 	@Override
 	public void read(MediusMessage mm) {
-		ByteBuffer buf = ByteBuffer.wrap(mm.getPayload());
-		buf.get(messageID);
-		buf.get(sessionKey);// buffer
-		buf.get(new byte[2]);
-		buf.get(characterEncoding);
-		buf.get(language);
-
-		logger.finest("Message ID : " + Utils.bytesToHex(messageID) + " | Length: " + Integer.toString(messageID.length));
+		reqPacket = new SetLocalizationParamsRequest(mm.getPayload());
+		logger.finest(reqPacket.toString());
 	}
 	
 	@Override
 	public void write(MediusClient client) {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		
-		try {
-			outputStream.write(Utils.buildByteArrayFromString("", 28));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-		client.sendMediusMessage(new MediusMessage(responseType, outputStream.toByteArray()));
+		MediusCallbackStatus callbackStatus = MediusCallbackStatus.SUCCESS;
+		
+		respPacket = new SetLocalizationParamsResponse(reqPacket.getMessageId(), callbackStatus);
+		
+		client.sendMediusMessage(respPacket);
 	}
 
 
