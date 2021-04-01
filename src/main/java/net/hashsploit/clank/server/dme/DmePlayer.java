d99c46b105b24470246a4cda66c4737c0d67fa39
hashsploit <hashsploit@protonmail.com>
2021-04-01 14:22:23 -0700

    Add udpclient in dmeplayer

--- src/main/java/net/hashsploit/clank/server/dme/DmePlayer.java
+++ src/main/java/net/hashsploit/clank/server/dme/DmePlayer.java
@@ -3,16 +3,22 @@ package net.hashsploit.clank.server.dme;
 public class DmePlayer {
 	
 	private final DmeClient client;
-	private DmeUdpClient
+	private DmeUdpClient udpClient;
 	
 	public DmePlayer(DmeClient client) {
 		this.client = client;
 	}
 	
-	
-	
 	public DmeClient getClient() {
 		return client;
+	}
+	
+	public void setUdpClient(DmeUdpClient udpClient) {
+		this.udpClient = udpClient;
+	}
+	
+	public DmeUdpClient getUdpClient() {
+		return udpClient;
 	}
 	
 }
